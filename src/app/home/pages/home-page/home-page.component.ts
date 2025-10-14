a0f08414720002f04c9295b510cb99a22b0a8f07
AngelPoot <angelpootucan7@gmail.com>
2025-10-14 13:11:39 -0600

    Implement home page layout and integrate MatIconModule

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent { }
