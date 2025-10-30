ff39a5f59bb4e20eea990dd1e1dfd88c63a17dfc
AngelPoot <angelpootucan7@gmail.com>
2025-10-30 16:58:27 -0600

    Refactor credits dialog: remove old credits dialog component and integrate credits display in profile page with modal functionality.

--- src/app/profile/pages/profile-page/profile-page.component.ts
+++ src/app/profile/pages/profile-page/profile-page.component.ts
@@ -2,10 +2,16 @@ import { Component, computed, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
+import { DatePipe } from '@angular/common';
+
+interface CreditData {
+  currentBalance: number, 
+  currency: string,
+}
 
 @Component({
   selector: 'app-profile-page',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, DatePipe],
   templateUrl: './profile-page.component.html',
 })
 export class ProfilePageComponent { 
@@ -13,4 +19,10 @@ export class ProfilePageComponent {
 
   user = computed(() => this.authService.user());
 
+  now = new Date();
+
+  creditData: CreditData = {
+    currentBalance: 150.75,
+    currency: 'MEX',
+  }
 }
