4ee832dc2e9ea16c93b44c029d7f5768695fa26d
AngelPoot <angelpootucan7@gmail.com>
2025-11-07 20:40:14 -0600

    Implement sales data table in dash sale page with product details and actions.

--- src/app/dashboard/pages/dash-sale-page/dash-sale-page.component.ts
+++ src/app/dashboard/pages/dash-sale-page/dash-sale-page.component.ts
@@ -5,4 +5,40 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './dash-sale-page.component.html',
 })
-export class DashSalePageComponent { }
+export class DashSalePageComponent { 
+
+  sales =  [
+    {
+      imageUrl: 'https://img.freepik.com/psd-gratis/refrescante-bebida-cola-fria-vaso-salpicaduras_632498-25634.jpg?semt=ais_hybrid&w=740&q=80',
+      name: 'Refrescante Bebida Cola',
+      price:  20.00,
+      stock: 50,
+      sold: 200, 
+      category: 'Bebidas'
+    },
+    {
+      imageUrl: 'https://png.pngtree.com/png-vector/20210630/ourmid/pngtree-t-shirt-clothes-casual-shirt-png-image_3544180.jpg',
+      name: 'Camiseta Oficial Evento',
+      price:  150.00,
+      stock: 70,
+      sold: 50, 
+      category: 'Ropa'
+    },
+    {
+      imageUrl: 'https://img.freepik.com/psd-gratis/refrescante-bebida-cola-fria-vaso-salpicaduras_632498-25634.jpg?semt=ais_hybrid&w=740&q=80',
+      name: 'Refrescante Bebida Cola',
+      price:  20.00,
+      stock: 50,
+      sold: 200, 
+      category: 'Bebidas'
+    },
+    {
+      imageUrl: 'https://png.pngtree.com/png-vector/20210630/ourmid/pngtree-t-shirt-clothes-casual-shirt-png-image_3544180.jpg',
+      name: 'Camisa Oficial Evento',
+      price:  150.00,
+      stock: 70,
+      sold: 50, 
+      category: 'Ropa'
+    },
+  ]
+}
